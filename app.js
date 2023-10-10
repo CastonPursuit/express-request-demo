30fd652e4022a4e5ec38821c15052b63bf179c8e
Gary Kertis <Gary>
2023-10-10 11:14:14 -0400

    Show & Create routes and middelware

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const app = express();
 const colorsController = require('./controllers/colorsController.js');
 const shapesController = require('./controllers/shapesController.js');
 
+// !!! IMPORTANT. If you are seeing req.body as undefined in your console.log
+// statements, you probably forgot this line:
+app.use(express.json()); // parse incoming middleware
+
+// The logger.
+
 app.use('/colors', colorsController);
 app.use('/shapes', shapesController);
 
@@ -12,3 +18,10 @@ app.get('*', (req, res) => {
 });
 
 module.exports = app;
+
+// /addNewColor?newColorName=tomato
+// /updateExistingColor?id=2
+// /deleteExistingColor?id=2
+
+// ^ Not super maintainable. What if I want to change something? Like I want to have colors become an object.
+// with multiple keys... { colorname: tomato, rating: 5 }
--- controllers/colorsController.js
+++ controllers/colorsController.js
@@ -2,12 +2,55 @@ const express = require('express');
 const colors = express.Router();
 const colorsArray = require('../models/color.js');
 
+// INDEX ROUTE:
+// We are showing the entire list of items!
 colors.get('/', (req, res) => {
   res.json(colorsArray);
 });
 
-colors.get('/foo', (req, res) => {
-  res.send('Hi there colors!');
+colors.get('/somethingElse', (req, res, next) => {
+  res.send('Hi');
 });
+
+// SHOW ROUTE!
+// WE ARE SHOWING ONE ITEM FROM OUR LIST
+colors.get('/:myId', (req, res) => {
+  const { myId } = req.params;
+  if (colorsArray[myId]) {
+    res.send(colorsArray[myId]);
+  } else {
+    res.status(404).send('Not found!');
+  }
+});
+
+colors.use((req, res, next) => {
+  console.log(req.method, req.headers.host, req.path);
+  if (req.body.userName === 'Gary' && req.body.password === '123') {
+    return next();
+  } else {
+    return res.status(403).send('No no no!');
+  }
+});
+
+const checkForColorKey = (req, res, next) => {
+  console.log('Check');
+  if (req.body.hasOwnProperty('name')) {
+    return next();
+  } else {
+    res.send('You must supply an object with a key of `name`');
+  }
+};
+
+// CREATE ROUTE! Creates a new resource on the server.
+colors.post('/', checkForColorKey, (req, res) => {
+  const { name } = req.body;
+  colorsArray.push(name);
+  res.send('Ok');
+});
+
+// colors.get('/somethingElse', (req, res, next) => {
+//   console.log('Something else route');
+//   res.send('Hi');
+// });
 
 module.exports = colors;
